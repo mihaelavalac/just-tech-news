d15225bc1bd8be3d33eedb891228521d2ddffb16
Mihaela Valac <mvalac@student.cscc.edu>
2022-01-24 14:05:06 -0500

    Test

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
