e45d7af83cf0932d33ec5eb7e0a7876d68156bb8
Mihaela Valac <mvalac@student.cscc.edu>
2022-01-29 20:29:35 -0500

    Added the sessio functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,21 +2,8 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
-// router.get('/', (req, res) => {
-//   res.render('homepage', {
-//     id: 1,
-//     post_url: 'https://handlebarsjs.com/guide/',
-//     title: 'Handlebars Docs',
-//     created_at: new Date(),
-//     vote_count: 10,
-//     comments: [{}, {}],
-//     user: {
-//       username: 'test_user'
-//     }
-//   });
-// });
-
 router.get("/", (req, res) => {
+  console.log(req.session);
   Post.findAll({
     attributes: [
       "id",
@@ -48,8 +35,8 @@ router.get("/", (req, res) => {
     .then((dbPostData) => {
       // pass a single post object into the homepage template
       //console.log(dbPostData[0]);
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { posts });
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
     })
     .catch((err) => {
       console.log(err);
@@ -57,8 +44,13 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
