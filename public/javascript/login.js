38449706768a9ea86195ae59ba767f528746bf7e
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-02 15:37:07 -0500

    Created the dashboard route with its form.

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -46,7 +46,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/dashboard");
       console.log("Success!")
     } else {
       alert(response.statusText);
